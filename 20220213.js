f66be0741697954f50bf566375b1eee68b94852b
Felice Forgione <forgione67@yahoo.com>
2022-02-13 12:06:56 -0500

    Create 20220213.js

---
+++ 20220213.js
@@ -0,0 +1,5 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c) {
+  return String.fromCharCode(c);
+}
