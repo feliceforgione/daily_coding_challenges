19ae46ab82aea7abc2fd53c6890ba1b0ade3a8a3
Felice Forgione <forgione67@yahoo.com>
2022-03-01 20:04:45 -0500

    Create 20220

---
+++ 20220301.js
@@ -0,0 +1,10 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// P: number
+// R: number
+// E:   (3) => 9
+// PC: multiply number with itself and return
+
+const square = (num) => {
+  return num * num;
+};
