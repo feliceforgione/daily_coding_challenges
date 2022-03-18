516e08028aaf79624d2abf8ede04526f89276bf3
Felice Forgione <forgione67@yahoo.com>
2022-03-18 00:50:36 -0400

    Create 20220318.js

---
+++ 20220318.js
@@ -0,0 +1,7 @@
+/* Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular." */
+
+var Ball = function (ballType) {
+  this.ballType = ballType || "regular";
+};
