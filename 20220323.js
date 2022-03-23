9fd5aef53cf4c780a0232098f83a2d0e18639201
Felice Forgione <forgione67@yahoo.com>
2022-03-23 01:11:58 -0400

    Create 20220323.js

---
+++ 20220323.js
@@ -0,0 +1,19 @@
+/* Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more. */
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) return "drink toddy";
+  if (old < 18) return "drink coke";
+  if (old < 21) return "drink beer";
+  return "drink whisky";
+}
