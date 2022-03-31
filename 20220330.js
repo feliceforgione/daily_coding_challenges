577158b171c51ba140772a7867f8273114515ae3
Felice Forgione <forgione67@yahoo.com>
2022-03-31 01:24:34 -0400

    Create 20220330.js

---
+++ 20220330.js
@@ -0,0 +1,11 @@
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+//P: integers
+//R: integer
+//PC: sum the three variables
+
+var laLigaGoals;
+var championsLeagueGoals;
+var copaDelReyGoals;
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
