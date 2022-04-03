5ecc151501dc86f9630687ae1cc519e9e2bae7ac
Felice Forgione <forgione67@yahoo.com>
2022-04-03 02:17:14 -0400

    Create 20220403.js

---
+++ 20220403.js
@@ -0,0 +1,14 @@
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase
+
+// P: string
+// R: string
+// E:   altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// PC:  iterate thru string
+
+String.prototype.toAlternatingCase = function () {
+  return [...this]
+    .map((c) => {
+      return c.toUpperCase() == c ? c.toLowerCase() : c.toUpperCase();
+    })
+    .join("");
+};
