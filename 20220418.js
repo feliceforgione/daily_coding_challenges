140bfa94c52fef0d010c00391b16e0c320f592ea
Felice Forgione <93881327+feliceforgione@users.noreply.github.com>
2022-04-20 00:11:18 -0400

    Create 20220418.js

---
+++ 20220418.js
@@ -0,0 +1,12 @@
+// Rock Paper Scissors
+
+const rps = (p1, p2) => {
+  if ( p1 === p2) {
+    return 'Draw!'
+  }
+  else if ((p1 === 'scissors' && p2 === 'paper') || (p1 === 'paper' && p2 === 'rock') || (p1 === 'rock' && p2 === 'scissors')) {
+    return 'Player 1 won!'
+  } else {
+    return 'Player 2 won!'
+  }
+};
