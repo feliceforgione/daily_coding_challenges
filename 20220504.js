eb91897ce199c07831c2c0e2fa525225b486af0f
Felice Forgione <forgione67@yahoo.com>
2022-05-05 02:35:20 -0400

    Create 20220504.js

---
+++ 20220504.js
@@ -0,0 +1,12 @@
+// Write a function, pairProduct, that takes in an array and a target product as arguments. The function should return an array containing a pair of indices whose elements multiply to the given target. The indices returned must be unique.
+const pairProduct = (numbers, targetProduct) => {
+    const hash = {}
+    for (let i = 0; i < numbers.length; i++) {
+       const num = numbers[i];
+       const comp = targetProduct/num 
+       if (comp in hash) {
+         return [hash[comp], i]
+       }
+       hash[num] = i;
+    }
+  }
