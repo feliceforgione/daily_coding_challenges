77bdc0ebba8266a4dd16a94066fd7c7972cfa25b
Felice Forgione <forgione67@yahoo.com>
2022-05-27 23:47:47 -0400

    Create 20220527.js

---
+++ 20220527.js
@@ -0,0 +1,9 @@
+/* Write a function, getNodeValue, that takes in the head of a linked list and an index. The function should return the value of the linked list at the specified index.
+
+If there is no node at the given index, then return null. */
+
+const getNodeValue = (head, index) => {
+  if (head === null) return null;
+  if (index === 0) return head.val;
+  return getNodeValue(head.next, index - 1);
+};
