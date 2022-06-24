5698b256f1d2c1c194e8ea183aaa374cf5af3485
Felice Forgione <forgione67@gmail.com>
2022-06-24 00:19:51 -0400

    Created 20220624.js

---
+++ 20220624.js
@@ -0,0 +1,17 @@
+/* Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+Note to Tax: not the invested principal is taxed, but only the year's accrued interest */
+
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
+  let investedSum = principal;
+  while (investedSum < desired) {
+    let profit = investedSum * interest;
+    profit = profit * (1 - tax);
+    investedSum = investedSum + profit;
+    years++;
+  }
+  return years;
+}
