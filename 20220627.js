46dfca7cfab1e64ef8e7671c4f3cfdbbf9a26c24
Felice Forgione <forgione67@gmail.com>
2022-06-27 00:20:56 -0400

    Created 20220627.js

---
+++ 20220627.js
@@ -0,0 +1,18 @@
+/* Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count. */
+
+function nbDig(n, d) {
+  let numDigits = 0;
+  for (let i = 0; i <= n; i++) {
+    let square = (i * i).toString();
+    for (let y = 0; y <= square.length; y++) {
+      if (square[y] == d) numDigits++;
+    }
+  }
+  return numDigits;
+}
