44f439ab9f4d014c640e83d4770b350d83d8ce50
Felice Forgione <forgione67@gmail.com>
2022-08-03 00:59:42 -0400

    Created 20220729.js

---
+++ 20220729.js
@@ -0,0 +1,32 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+
+const twoSumBruteForce = (nums, target) => {
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
+};
+
+const twoSum = (nums, target) => {
+  let complimentIndex = {};
+  for (let i = 0; i < nums.length; i++) {
+    const value = nums[i];
+    const compliment = target - value;
+    if (complimentIndex[value] == null) {
+      complimentIndex[compliment] = i;
+    } else {
+      return [complimentIndex[value], i];
+    }
+  }
+  return [];
+};
+
+console.log(twoSum([1, 2, 3, 4, 5], 6));
