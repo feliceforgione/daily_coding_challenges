7bb88fc8aa8b3c8188328166f8cf22a35dbfff3c
Felice Forgione <forgione67@gmail.com>
2022-09-11 20:51:01 -0400

    Created 20220830.js

---
+++ 20220830.js
@@ -0,0 +1,14 @@
+/* Complete the method which accepts an array of integers, and returns one of the following:
+
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+You can assume the array will always be valid, and there will always be one correct answer. */
+
+function isSortedAndHow(array) {
+  const arrayStr = array.join("");
+  const arraySort = array.sort((a, b) => a - b);
+  if (arrayStr === arraySort.join("")) return "yes, ascending";
+  if (arrayStr === arraySort.reverse().join("")) return "yes, descending";
+  return "no";
+}
