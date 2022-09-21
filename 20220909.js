b450faddcd1bd58cd64ef92c445b9ec02179f162
Felice Forgione <forgione67@gmail.com>
2022-09-21 02:11:04 -0400

    Created 20220909.js

---
+++ 20220909.js
@@ -0,0 +1,21 @@
+/* DESCRIPTION:
+Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+Example 1:
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+
+Example 2:
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns [] */
+
+function inArray(array1, array2) {
+  const whole = array2.join(" ");
+  return array1.filter((word) => whole.includes(word)).sort();
+}
