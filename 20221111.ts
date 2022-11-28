a2d91e6a5db8781aea439e099af9f1e9554511d0
Felice Forgione <forgione67@gmail.com>
2022-11-28 04:52:14 -0500

    Created 20221111.ts

---
+++ 20221111.ts
@@ -0,0 +1,17 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n: number): number {
+  const reversedStr: string = n.toString().split("").reverse().join("");
+
+  return Math.sign(n) * parseInt(reversedStr);
+}
+
+console.log(reverseInt(-15));
