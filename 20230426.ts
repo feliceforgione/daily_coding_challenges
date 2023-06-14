e593dff4fbae6900d6d08449504c65f3710ea39e
Felice Forgione <forgione67@gmail.com>
2023-06-13 20:31:30 -0400

    Created 20230426.ts

---
+++ 20230426.ts
@@ -0,0 +1,20 @@
+/* Create a function makeFuncTester that accepts an array (of two-element sub-arrays), and returns a function (that will accept a callback). The returned function should return true if the first elements (of each sub-array) being passed into the callback all yield the corresponding second elements (of the same sub-array). Otherwise, the returned function should return false. */
+
+type BooleanFunction = (A: (a: string) => string) => boolean;
+
+function makeFuncTester(arrOfTests): BooleanFunction {
+  function compare(cb) {
+    return arrOfTests.every((test) => cb(test[0]) == test[1]);
+  }
+  return compare;
+}
+
+const capLastTestCases: string[][] = [];
+capLastTestCases.push(["hello", "hellO"]);
+capLastTestCases.push(["goodbye", "goodbyE"]);
+capLastTestCases.push(["howdy", "howdY"]);
+const shouldCapitalizeLast = makeFuncTester(capLastTestCases);
+const capLastAttempt1 = (str) => str.toUpperCase();
+const capLastAttempt2 = (str) => str.slice(0, -1) + str.slice(-1).toUpperCase();
+console.log(shouldCapitalizeLast(capLastAttempt1)); // => should log false
+console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
