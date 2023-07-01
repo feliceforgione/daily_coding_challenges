0d62da17ccf7005c97dfa6404573f7800eeb4ff4
Felice Forgione <forgione67@gmail.com>
2023-06-30 23:49:38 -0400

    Created 20230514.ts

---
+++ 20230514.ts
@@ -0,0 +1,33 @@
+/* Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ */
+
+function merge(intervals: number[][]): number[][] {
+  if (intervals.length < 2) return intervals;
+  const result: number[][] = [];
+  intervals.sort((a, b) => a[0] - b[0]);
+  let current = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (current[1] < intervals[i][0]) {
+      result.push(current);
+      current = intervals[i];
+    } else {
+      current[1] = Math.max(current[1], intervals[i][1]);
+    }
+  }
+  result.push(current);
+  return result;
+}
